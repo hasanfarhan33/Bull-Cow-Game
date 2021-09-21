cac227d5451e7118483a54ebd332821b17e427de
Farhan Hasan <hasanfarhan33@gmail.com>
2021-09-21 21:41:35 +0300

    Initial Commit

---
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "BullCowCartridge.h"
+
+void UBullCowCartridge::BeginPlay() // When the game starts
+{
+    Super::BeginPlay();
+    PrintLine(TEXT("Welcome to the Bull Cows game."));
+    PrintLine(TEXT("Press Enter to continue"));
+}
+
+void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
+{
+    ClearScreen();
+    FString HiddenWord = TEXT("cake");
+    if (Input == HiddenWord)
+    {
+        PrintLine(TEXT("You have won!"));
+    }
+    else
+    {
+        PrintLine(TEXT("You lose!"));
+    }
+}
