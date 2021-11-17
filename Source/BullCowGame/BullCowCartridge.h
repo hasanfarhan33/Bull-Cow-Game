9b5b8eb0ea0051f45bd7778c5e741a8394c86135
Farhan Hasan <hasanfarhan33@gmail.com>
2021-11-17 22:30:08 +0300

    Another commit again, bug

--- Source/BullCowGame/BullCowCartridge.h
+++ Source/BullCowGame/BullCowCartridge.h
@@ -15,9 +15,11 @@ public:
 	virtual void BeginPlay() override;
 	virtual void OnInput(const FString &Input) override;
 	void SetupGame();
+	void EndGame();
 
 	// Your declarations go below!
 private:
 	FString HiddenWord;
 	int32 Lives;
+	bool bGameOver;
 };
