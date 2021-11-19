af693df596661c87c3799308c624286e30053f2e
Farhan Hasan <hasanfarhan33@gmail.com>
2021-11-19 23:15:42 +0300

    Cleaned the if else statements using early returns

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -56,7 +56,7 @@ void UBullCowCartridge::SetupGame()
 void UBullCowCartridge::EndGame()
 {
     bGameOver = true;
-    PrintLine(TEXT("Press enter to play again"));
+    PrintLine(TEXT("\nPress enter to play again"));
 }
 
 void UBullCowCartridge::ProcessGuess(FString Guess)
@@ -65,24 +65,37 @@ void UBullCowCartridge::ProcessGuess(FString Guess)
     {
         PrintLine(TEXT("You have won!"));
         EndGame();
+        return;
     }
-    else
-    {
-        --Lives;
-        PrintLine(TEXT("Lost a life"));
-        if (Lives > 0)
-        {
-            PrintLine(TEXT("%i lives remaining"), Lives);
-        }
 
-        if (Lives > 0)
-        {
-            PrintLine(TEXT("Try again, you have %i lives remaining"), Lives);
-        }
-        else
-        {
-            PrintLine(TEXT("You have no lives left"));
-            EndGame();
-        }
+    // Check if isogram
+    if (!IsIsogram)
+    {
+        PrintLine(TEXT("No repeating letters, guess again"));
     }
+
+    // Check the right number of characters
+    if (Guess.Len() != HiddenWord.Len())
+    {
+        PrintLine(TEXT("Sorry, try again. \nYou have %i lives remaining"), Lives);
+        PrintLine(TEXT("The hidden word is %i letters long"), HiddenWord.Len());
+        return;
+    }
+
+    // Remove a life
+    PrintLine(TEXT("You have lost a life"));
+    --Lives;
+
+    if (Lives <= 0)
+    {
+        ClearScreen();
+        PrintLine(TEXT("You have no lives left!"));
+        PrintLine(TEXT("The hidden word was: %s"), *HiddenWord);
+
+        EndGame();
+        return;
+    }
+
+    // Show the player the bulls and the cows
+    PrintLine(TEXT("Guess again, you have %i lives left"), Lives);
 }
